130c910f3abf2aba94fcfbfad5b3e2f95eee1b40
Mike MA <594mjf@gmail.com>
2017-01-11 15:55:56 +0800

    init the project
    This is the inital point of a vue project.

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <navHeader></navHeader>
+    <el-button @click.native="startHacking">Let's do it</el-button>
+    <el-row :gutter="10">
+      <el-col :xs="8" :sm="6" :md="4" :lg="3">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :xs="4" :sm="6" :md="8" :lg="9">
+        <div class="grid-content bg-purple-light"></div>
+      </el-col>
+      <el-col :xs="4" :sm="6" :md="8" :lg="9">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :xs="8" :sm="6" :md="4" :lg="3">
+        <div class="grid-content bg-purple-light"></div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import navHeader from "./components/navHeader";
+export default {
+  name:"app",
+  data() {
+    return {
+      msg: 'Use Vue 2.0 Today!',
+    };
+  },
+  components: {
+    navHeader
+  },
+  methods: {
+    startHacking() {
+      this.$notify({
+        title: 'It Works',
+        message: 'We have laid the groundwork for you. Now it\'s your time to build something epic!',
+        duration: 6000,
+      });
+    }
+  }
+}
+
+
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+
+
+</style>
